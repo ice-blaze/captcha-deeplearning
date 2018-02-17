01c239b75337aed9ad425ab74685019ca06df466
ice-blaze <franketip@gmail.com>
2018-02-17 10:23:06 +0100

    Add lines

--- generate_captchas.py
+++ generate_captchas.py
@@ -5,6 +5,7 @@ import os, errno
 import random
 import itertools
 import uuid
+import random
 
 CHAR_POSSIBILITIES = "0123456789abcdefghijklmnopqrstuvwxyz"
 CHAR_COUNT = 5
@@ -21,6 +22,18 @@ def get_random_names(how_many=1, length=1, possibilities="ab"):
     for number in range(0, how_many):
         random_name = ''.join(random.SystemRandom().choice(possibilities) for _ in range(length))
         yield random_name
+
+
+def random_line(draw, color):
+    all_lines = [
+        [(30,48), (34,25), (76,15), (259,23)],
+        [(30,1), (32,9), (40,15), (55,23), (147,35), (271,33)],
+        [(39,36), (42,17), (95,22), (271,31)],
+        [(30,16), (36,26), (65,25), (143,16), (205,15), (271,18)],
+    ]
+
+    random_index = random.randint(0, len(all_lines) - 1)
+    draw.line(all_lines[random_index], fill=color, width=3)
 
 
 def generate_captchas(
@@ -42,22 +55,23 @@ def generate_captchas(
 
     random_names = get_random_names(how_many, char_count, char_possibilities)
     CHAR_WIDTH_DELTA = 5
-    start_x = 16
     START_Y = -4
     font = ImageFont.truetype("./fonts/Frutiger-Black.otf", 42)
 
-
     base_image = Image.open(base_image)
+    color = (0, 0, 0)
 
     for name in random_names:
+        start_x = 16
         base = base_image.copy()
         draw = ImageDraw.Draw(base)
         for letter in name:
-            draw.text((start_x, START_Y ), letter, (0, 0, 0), font=font)
+            draw.text((start_x, START_Y ), letter, color, font=font)
             size = font.getsize(letter)[0]
             start_x += size - CHAR_WIDTH_DELTA
 
         # TODO draw a line
+        random_line(draw, color)
 
         id = str(uuid.uuid4()).replace("-", "")
         base.save(output_path + name + '-' + id + '.png')
